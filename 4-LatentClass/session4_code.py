acf9292b6b18a11610eb901d273810cb361f7609
JingpengHong <jphong0403@outlook.com>
2022-03-08 18:53:13 -0600

    Session 4 Started

---
+++ 4-LatentClass/session4_code.py
@@ -0,0 +1,13 @@
+'''
+Foundation of Advanced Quantitative Marketing
+Seesion 4: 
+    1) Observable heterogeneity models
+    2) State-dependence models
+    3) Latent class logit model.
+Data: Yogurt_Data.csv
+Author: Jingpeng Hong
+Instructor: Pradeep Chintagunta
+'''
+##################
+## Housekeeping ##
+##################
