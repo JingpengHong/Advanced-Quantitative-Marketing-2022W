5c0adf52c166c574d6cc320404ff08d1861ebcd3
JingpengHong <jphong0403@outlook.com>
2022-03-13 18:51:31 -0500

    BLP started

---
+++ 6-BLP/session6_code.py
@@ -0,0 +1,42 @@
+'''
+Foundation of Advanced Quantitative Marketing
+Seesion 6: 
+    1) BLP: Aggregate Level Data
+Data: Coffee_Data.csv
+Author: Jingpeng Hong
+Instructor: Pradeep Chintagunta
+'''
+
+##################
+## Housekeeping ##
+##################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
